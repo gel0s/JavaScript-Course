6a69b151ddb2c17c98b318aae8f122634d9799cc
Ануша Хайруллоева <anusha.khayrulloeva@dc.alif.tj>
2022-11-07 19:03:15 +0500

    increment and decriment

--- Operations/index.js
+++ Operations/index.js
@@ -1,3 +1,29 @@
 // 1) Mathematical operations
 
 // Деление по модулю (оператор %) возвращает остаток от деления:
+let x = 5;
+let y = 2;
+let z = x % y;
+console.log(z); // 1
+
+// Возведение в степень. Оператор ** возводит число в определенную степень:
+let a = 5
+console.log(a**2);
+
+
+// Инкремент
+let c = 6;
+c++
+console.log(c);
+c++
+c++
+c++
+c++
+console.log(c);
+
+// Декремент 
+c--
+let f = c-- 
+c--
+c--
+console.log(f);
