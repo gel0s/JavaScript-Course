84431bde3eb6394290d093f2901130e584ac34b5
Ануша Хайруллоева <anusha.khayrulloeva@dc.alif.tj>
2022-11-07 18:09:24 +0500

    added Data types

---
+++ Data-Types/index.js
@@ -0,0 +1,27 @@
+// 1) String
+let age = "20" // String
+
+// convertation types
+let newage = Number(age) //Number
+
+// 2) Number
+
+// let age = 20 // Number
+
+// let name = "Salom"+20 //Salom20 
+
+console.log("type of age before convert: "+ typeof(age))
+console.log("type of age after convert: "+ typeof(newage))
+
+// console.log("name: ", name);
+
+// 3) Boolean
+
+// true
+// false
+
+// 4) Array
+
+let students = [
+    "Anusha"
+]
